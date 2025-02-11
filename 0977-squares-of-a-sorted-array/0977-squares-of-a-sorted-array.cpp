d28d48a458c72173de3fef805a56f29ee4cebd0b
Prabhakar Patil <161556007+RVCEprabhakar@users.noreply.github.com>
2025-02-11 22:00:31 +0530

    Time: 7 ms (35.73%), Space: 31.1 MB (25.83%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -4,9 +4,14 @@ public:
         int n=nums.size();
         vector<int>ans;
         for(int i=0;i<n;i++){
+            
             ans.push_back({nums[i]*nums[i]});
+            
+
         }
         sort(ans.begin(),ans.end());
-    return ans;
+        return ans;
+        
+        
     }
 };
