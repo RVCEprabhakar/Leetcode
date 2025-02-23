d618d701ac480d942e7cf8be7b253f8d5acd79f0
Prabhakar Patil <161556007+RVCEprabhakar@users.noreply.github.com>
2025-02-23 17:08:47 +0530

    Time: 5 ms (27.63%), Space: 17.9 MB (47.5%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // Min-heap to keep track of top k frequent elements
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minh;
-        unordered_map<int,int> mpp;  // To count the frequency of each element
-        vector<int> result;
-
-        // Count the frequency of each element in nums
-        for(int i = 0; i < nums.size(); i++) {
-            mpp[nums[i]]++;
+        unordered_map<int, int> mpp;
+        for (int num : nums) {
+            mpp[num]++;
         }
 
-        // Iterate over the frequency map
-        for(auto i = mpp.begin(); i != mpp.end(); i++) {
-            minh.push({i->second, i->first});  // Push frequency and element pair into min-heap
-            if(minh.size() > k) {
-                minh.pop();  // Keep only the top k elements in the min-heap
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+        for (auto it : mpp) {
+            heap.push({it.second, it.first});
+            if (heap.size() > k) {
+                heap.pop();
             }
         }
 
-        // Extract elements from the min-heap
-        while(!minh.empty()) {
-            result.push_back(minh.top().second);  // Get the element (second in the pair)
-            minh.pop();
+        vector<int> res;
+        for (int i = 0; i < k; i++) {
+            res.push_back(heap.top().second);
+            heap.pop();
         }
-
-        return result;
+        return res;
     }
 };
+
