925d84bb71033da291fa9ee0ae608867f640cac8
Prabhakar Patil <161556007+RVCEprabhakar@users.noreply.github.com>
2025-03-11 08:05:01 +0530

    Time: 3 ms (32.44%), Space: 9.9 MB (57.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int n=s.length();
-        int left=0;
-        int right=n-1;
-        while(left<right){
-            if(!isalnum(s[left])){
+        int n = s.length();
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            if (!isalnum(s[left])) {
                 left++;
                 continue;
-            }
-            else if(!isalnum(s[right])){
+            } else if (!isalnum(s[right])) {
                 right--;
                 continue;
-            }
-            else if(tolower(s[left])!=tolower(s[right]))
-            return false;
-            else{
-                 left++;
+            } else if (tolower(s[left]) != tolower(s[right]))
+                return false;
+            else {
+                left++;
                 right--;
-
             }
- 
-
         }
         return true;
-
-        
     }
 };
