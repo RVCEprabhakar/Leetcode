c569244c3bcb3aeb69b1b0d7a1eb5c6ad7b92fd0
Prabhakar Patil <161556007+RVCEprabhakar@users.noreply.github.com>
2025-04-17 10:34:56 +0530

    Time: 0 ms (100%), Space: 7.6 MB (98.15%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numTrees(int n) {
+
+        long long res=1;
+        for(int i=1;i<=n;i++){
+            res*=(n+i);
+            res/=i;
+        }
+        return res/(n+1);
+        
+    }
+};
