c56618c1410c3a5daee7af6f0f96a935d65abffb
Prabhakar Patil <161556007+RVCEprabhakar@users.noreply.github.com>
2026-01-12 09:07:07 +0530

    Time: 1 ms (64.76%), Space: 43 MB (85.21%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,22 +4,19 @@ class Solution {
  if (strs == null || strs.length == 0) {
             return "";
         }
-
-        // Loop through characters of first string
+        
         for (int i = 0; i < strs[0].length(); i++) {
             char ch = strs[0].charAt(i);
 
-            // Compare with all other strings
             for (int j = 1; j < strs.length; j++) {
 
-                // If index exceeds length OR characters mismatch
                 if (i >= strs[j].length() || strs[j].charAt(i) != ch) {
                     return strs[0].substring(0, i);
                 }
             }
         }
 
-        // Entire first string is common prefix
+        
         return strs[0];
 
 
