b2df5a6911025e65fae1de5d55ea4abd70b6a67c
Prabhakar Patil <161556007+RVCEprabhakar@users.noreply.github.com>
2026-03-10 09:35:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,43 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length()==t.length()){
+//             char[] s1=s.toCharArray();
+//             char[] s2=t.toCharArray();
+//              Arrays.sort(s1);
+//              Arrays.sort(s2);
+//              if(Arrays.equals(s1,s2)){
+//                 return true;
+//              }
+//              else{
+//                 return false;
+//              }
+
+//         }else{
+//             return false;
+//         }
+        
+//     }
+// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()==t.length()){
-            char[] s1=s.toCharArray();
-            char[] s2=t.toCharArray();
-             Arrays.sort(s1);
-             Arrays.sort(s2);
-             if(Arrays.equals(s1,s2)){
-                return true;
-             }
-             else{
-                return false;
-             }
 
-        }else{
+        if(s.length()!=t.length())
             return false;
+
+        int[] count = new int[26];
+
+        for(char c : s.toCharArray()){
+            count[c - 'a']++;
         }
-        
+
+        for(char c : t.toCharArray()){
+            count[c - 'a']--;
+
+            if(count[c - 'a'] < 0)
+                return false;
+        }
+
+        return true;
     }
 }
