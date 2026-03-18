815ec282581219d9f618615cd676ca656ae91a66
Prabhakar Patil <161556007+RVCEprabhakar@users.noreply.github.com>
2026-03-18 08:40:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,12 +1,13 @@
 class Solution {
   public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    
     int[] result = new int[nums1.length];
 
     for (int i = 0; i < nums1.length; i++) {
         int num = nums1[i];
         int index = -1;
 
-        // find index of num in nums2
+
         for (int j = 0; j < nums2.length; j++) {
             if (nums2[j] == num) {
                 index = j;
@@ -14,7 +15,7 @@ class Solution {
             }
         }
 
-        // find next greater
+
         int nextGreater = -1;
         for (int j = index + 1; j < nums2.length; j++) {
             if (nums2[j] > num) {
